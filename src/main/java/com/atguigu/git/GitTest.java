df2d643f4725f859011ecae48f0be9b5bbe8f833
kq <kq@qq.com>
2022-09-14 10:31:22 +0800

    hot-fix 2 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("22222");
         System.out.println("333333");
         System.out.println("hot-fix ");
+        System.out.println("hot-fix 2");
     }
 
 }
