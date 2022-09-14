0b0004b7bff0bd417aa0e754f07cce2b0b9a434e
kq <kq@qq.com>
2022-09-14 10:15:54 +0800

    3 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("22222");
+        System.out.println("333333");
     }
 
 }
